4dec9f419178edd620ce1d493987d5dd47a2138f
Nath <nathanielatlan@gmail.com>
2023-09-24 14:57:24 +0200

    Routes suite

--- routes/v1/games.js
+++ routes/v1/games.js
@@ -22,6 +22,59 @@ router.post("/", authentication, async (req, res) => {
   }
 });
 
-// D'autres routes pour rejoindre une partie, faire un mouvement, etc.
+router.post("/:id/join", authentication, async (req, res) => {
+  try {
+    const game = await Game.findById(req.params.id);
+    if (!game) return res.status(404).send("Game not found");
+    if (game.player2) return res.status(400).send("Game already full");
+    game.player2 = req.user._id;
+    await game.save();
+    res.send(game);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.put("/:id/move", authentication, async (req, res) => {
+  try {
+    const { x, y } = req.body;
+    const game = await Game.findById(req.params.id);
+    if (!game) return res.status(404).send("Game not found");
+    if (![game.player1, game.player2].includes(req.user._id.toString()))
+      return res.status(401).send("Unauthorized move");
+    if (game.board[x][y] !== " ") return res.status(400).send("Invalid move");
+    game.board[x][y] = game.currentPlayer;
+    game.currentPlayer = game.currentPlayer === "X" ? "O" : "X";
+    await game.save();
+    res.send(game);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get("/:id/status", authentication, async (req, res) => {
+  try {
+    const game = await Game.findById(req.params.id);
+    if (!game) return res.status(404).send("Game not found");
+    res.send({
+      board: game.board,
+      currentPlayer: game.currentPlayer,
+      status: game.status,
+    });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get("/", authentication, async (req, res) => {
+  try {
+    const games = await Game.find({
+      $or: [{ player1: req.user._id }, { player2: req.user._id }],
+    });
+    res.send(games);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 module.exports = router;
